024b28eff4dc49ca86971b1b7270fc9efc96baaa
Adrian <tudorache_adrian24@yahoo.com>
2019-11-09 15:56:42 +0200

    Create config.h
    https://github.com/axelerator/esp8266_ir_blaster?files=1

---
+++ config.h
@@ -0,0 +1,14 @@
+// All neccessary configuration parameters are defined in this file.
+// Copy it to 'config.h' and make the neccessary changes there.
+
+// Replace the ... with the name and password of your WiFi
+#define SSID "SSID"
+#define PASSWORD "pass"
+
+// An IR detector/demodulator is connected to GPIO pin 14(D5 on a NodeMCU
+// board).
+#define RECV_PIN 2
+
+// An IR LED is connected to GPIO pin 12 (D6 on a NodeMCU
+// board).
+#define IR_LED 12
